bf30d013228cbc4102aab167f4796b707d2ff203
Игорь Митяшин <ItsFreez@yandex.ru>
2023-10-17 20:09:39 +0300

    Create model Year

--- schedulum/schedules/admin.py
+++ schedulum/schedules/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+
+from schedules.models import Year
+
+admin.site.register(Year)
--- schedulum/schedules/models.py
+++ schedulum/schedules/models.py
@@ -1,1 +1,35 @@
+import datetime as dt
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+CURRENT_YEAR = dt.date.today().year
+NEXT_YEAR = dt.date.today().year + 1
+
+
+class Year(models.Model):
+    title = models.CharField(
+        max_length=15,
+        blank=True,
+        verbose_name='Заголовок',
+        help_text='Это поле автоматически заполнится, оставьте пустым.'
+    )
+    year = models.SmallIntegerField(
+        validators=[MinValueValidator(CURRENT_YEAR),
+                    MaxValueValidator(NEXT_YEAR)],
+        verbose_name='Год',
+        help_text=(
+            'Можно указать только текущий и следующий год. Обязательное поле.'
+        )
+    )
+
+    class Meta:
+        verbose_name = 'год'
+        verbose_name_plural = 'Годы'
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.title = str(self.year) + ' год'
+        return super().save(*args, **kwargs)
